4736863b04596f9fafc27168e77581d7124d7ab9
Karel Busta <karelbusta@gmail.com>
2025-09-20 08:02:46 +0200

    Add public top navigation

---
+++ src/components/public/public-top-nav.tsx
@@ -0,0 +1,40 @@
+import { Button } from '@/components/ui/button';
+import { auth } from '@/server/auth';
+import { headers } from 'next/headers';
+import Link from 'next/link';
+
+export async function PublicTopNav({
+  organizationName,
+  organizationSlug,
+}: {
+  organizationName: string;
+  organizationSlug: string;
+}) {
+  let isDashboardButtonVisible = false;
+  try {
+    const orgs = await auth.api.listOrganizations({ headers: await headers() });
+    isDashboardButtonVisible = !!orgs.find(
+      (org) => org.slug === organizationSlug
+    );
+  } catch (error) {
+    console.error(error);
+  }
+
+  return (
+    <div className="flex items-center justify-between mb-4">
+      <Link
+        href={`/${organizationSlug}/feedback`}
+        className="text-base font-medium hover:underline"
+      >
+        {organizationName || organizationSlug}
+      </Link>
+      {isDashboardButtonVisible ? (
+        <Link href={`/dashboard/${organizationSlug}/feedback`}>
+          <Button variant="outline" size="sm">
+            Dashboard
+          </Button>
+        </Link>
+      ) : null}
+    </div>
+  );
+}
